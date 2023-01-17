f00144b09a7b17f42e173b7abe0f258f6c9a5f5f
salam00-pp <shahbaz.alam@bhnetwork.com>
2023-01-17 10:47:19 +0530

    updating auth service

--- src/test/java/com/bhn/raptorauthorizationservice/RaptorAuthorizationServiceApplicationTests.java
+++ src/test/java/com/bhn/raptorauthorizationservice/RaptorAuthorizationServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bhn.raptorauthorizationservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RaptorAuthorizationServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.bhn.raptorauthorizationservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RaptorAuthorizationServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
